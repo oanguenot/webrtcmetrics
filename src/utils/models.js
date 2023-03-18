20b978a8ca4aa05a96b45d5d95809a661213a0f8
Olivier Anguenot <oanguenot@gmail.com>
2023-03-18 11:58:43 +0100

    Update version to publish uptimised version

--- src/utils/models.js
+++ src/utils/models.js
@@ -3,7 +3,7 @@ import ShortUniqueId from "short-unique-id";
 const shortUUID = new ShortUniqueId();
 
 export const getLibName = () => ("WebRTCMetrics");
-export const getVersion = () => ("5.3.2");
+export const getVersion = () => ("5.3.3");
 
 export const DIRECTION = {
   INBOUND: "inbound",
